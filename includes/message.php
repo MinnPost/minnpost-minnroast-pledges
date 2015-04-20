c601db612b0660daf42d82f259814edb2af9089b
Jonathan Stegall <jonathanstegall@MP-LT-23-MacAir.local>
2015-04-20 16:38:36 -0500

    success message

--- includes/message.php
+++ includes/message.php
@@ -1,4 +1,5 @@
 <section>
 	<h3 class="component-label">Thank you for your pledge</h3>
 	<p>We have received your pledge amount of <strong>&#36;<?php echo $amount; ?></strong>, under the email address <strong><?php echo $email; ?></strong>.</p>
+	<p>Within a few days, we will email that address with directions on how to fullfill your pledge.</p>
 </section>
