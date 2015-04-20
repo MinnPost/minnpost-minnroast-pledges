e223fc907c301d3152199c1d3b4ba2022ec18d40
Jonathan Stegall <jonathanstegall@MP-LT-23-MacAir.local>
2015-04-20 16:27:01 -0500

    ajax

--- pledges.js
+++ pledges.js
@@ -1,5 +1,10 @@
+function refreshTable(){
+    $('.results').load('data/table.php', function(){
+       setTimeout(refreshTable, 5000);
+    });
+}
 
-
-$(document).ready(function() {
-
+$(document).ready(function(){
+	$('.all-pledges').wrap('<div class="results"></div>');
+  refreshTable();
 });
