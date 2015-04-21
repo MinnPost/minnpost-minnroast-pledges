a56c5f6acb7549edd218db8c19045df812d42d76
Jonathan Stegall <jonathanstegall@MP-LT-23-MacAir.local>
2015-04-21 13:30:22 -0500

    update the send message

--- includes/message.php
+++ includes/message.php
@@ -1,5 +1,5 @@
 <section>
-	<h3 class="component-label">Thank you for your pledge</h3>
-	<p>We have received your pledge amount of <strong>&#36;<?php echo $amount; ?></strong>, under the email address <strong><?php echo $email; ?></strong>.</p>
-	<p>Within a few days, we will email that address with directions on how to fullfill your pledge.</p>
+	<h3 class="component-label">Thank you for helping us launch a New Americans beat!</h3>
+	<p>We have received your pledge of <strong>&#36;<?php echo $amount; ?></strong>, under the email address <strong><?php echo $email; ?></strong>.</p>
+	<p>Within a few days, we will email you at that address with options on how to fullfill your pledge.</p>
 </section>
