2ada2aab27c0e917470027331dba568cd6b014df
Jonathan Stegall <jstegall@minnpost.com>
2016-12-05 15:39:41 -0600

    where even are we?

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
-<?php require_once('/app/www/includes/head.php'); ?>
-<?php require_once('/app/www/includes/run.php'); ?>    
-<?php require_once('/app/www/includes/footer.php'); ?>
+<?php echo 'directory is ' . getcwd(); ?>
+<?php include('includes/head.php'); ?>
+<?php include('includes/run.php'); ?>    
+<?php include('includes/footer.php'); ?>
