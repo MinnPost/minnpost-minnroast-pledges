85265f6a56fdd394ffcfffc2a073ea477239c2a9
Jonathan Stegall <jstegall@minnpost.com>
2016-12-19 15:15:28 -0600

    deal with some more non integers

--- includes/pledges.php
+++ includes/pledges.php
@@ -1,14 +1,14 @@
 <table class="all-pledges">
-<?php $total = ''; $counter = 0; while($row = $result->fetch(PDO::FETCH_ASSOC)){ $total = $total + $row['amount']; ?>
+<?php $total = ''; $counter = 0; while($row = $result->fetch(PDO::FETCH_ASSOC)){ $total = (int) $total + (int) $row['amount']; ?>
 	<tr>
 		<td class="count"><?php echo ++$counter; ?></td>
-		<td class="amount"><strong>&#36;<?php echo number_format($row['amount']); ?></strong></td>
+		<td class="amount"><strong>&#36;<?php echo number_format((int) $row['amount']); ?></strong></td>
 		<td class="time"><?php echo get_timeago(strtotime($row['created'])); ?></td>
 	</tr>
 <?php } ?>
 	<tr>
 		<td class="count">Total</td>
-		<td class="amount"><strong>&#36;<?php echo number_format($total); ?></strong></td>
+		<td class="amount"><strong>&#36;<?php echo number_format((int) $total); ?></strong></td>
 		<td class="time">&nbsp;</td>
 	</tr>
 </table>
