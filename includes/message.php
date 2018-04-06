cf1cd05a34f362ea9c59bc2d8bdcd506b7815b7f
Jonathan Stegall <jstegall@minnpost.com>
2018-04-06 13:42:51 -0500

    fix spelling

--- includes/message.php
+++ includes/message.php
@@ -4,6 +4,6 @@
 	<?php if ($charge_if_on_file == 1) { ?>
 	<p>Early next week, we will attempt to charge your credit card on file. You will get a receipt email at that address if it is successful. We'll be in touch to fulfill your pledge if it is not on file.</p>
 	<?php } else { ?>
-	<p>Early next week, we will email you at that address with options on how to fullfill your pledge.</p>
+	<p>Early next week, we will email you at that address with options on how to fulfill your pledge.</p>
 	<?php } ?>
 </section>
