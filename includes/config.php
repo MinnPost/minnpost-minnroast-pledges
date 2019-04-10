69f9850ab534327fad75cde33e780394f1ebff9b
Jonathan Stegall <jstegall@minnpost.com>
2019-04-10 13:28:59 -0500

    i think that doesn't work on heroku

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
-if (file_exists( get_include_path() . '.env')) {
+if (file_exists('.env')) {
 	$dotenv->load();
 }
 
