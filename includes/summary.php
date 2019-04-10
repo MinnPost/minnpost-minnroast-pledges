dc74dfdaa769f69a8f4d52b8cb69b9f7eda07efa
Jonathan Stegall <jstegall@minnpost.com>
2019-04-10 12:41:51 -0500

    remove count of pledges

--- includes/summary.php
+++ includes/summary.php
@@ -1,6 +1,6 @@
 <section class="board">
 	<div class="total-pledges">
-		<strong><?php echo $count; ?></strong> <?php echo $word; ?> &mdash; &#36;<?php echo number_format( $total ); ?>
+		<strong>&#36;<?php echo number_format( $total ); ?></strong>
 	</div>
 	<?php if ( ! empty( $name ) ) : ?>
 		<div class="name">
