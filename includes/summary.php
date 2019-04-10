0bf5eb57f9a53ec8bc7cbe8852e4d30df574609d
Jonathan Stegall <jstegall@minnpost.com>
2019-04-10 11:33:19 -0500

    display name field if there is one, in a fade-in

--- includes/summary.php
+++ includes/summary.php
@@ -1,3 +1,10 @@
-<div class="total-pledges">
-  <div><strong><?php echo $count; ?></strong> <?php echo $word; ?> &mdash; &#36;<?php echo number_format($total); ?></div>
-</div>
+<section class="board">
+	<div class="total-pledges">
+		<strong><?php echo $count; ?></strong> <?php echo $word; ?> &mdash; &#36;<?php echo number_format( $total ); ?>
+	</div>
+	<?php if ( ! empty( $name ) ) : ?>
+		<div class="name">
+			<?php echo $name; ?>
+		</div>
+	<?php endif; ?>
+</section>
